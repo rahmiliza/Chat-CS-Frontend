5d0c04c87a3e0a5357bfc61db9ef69c59f947034
Rahmi Liza <57374297+rahmiliza@users.noreply.github.com>
2025-04-23 16:26:30 +0800

    Add chat components for active chat, chat bubble, queue counter, and room info

---
+++ components/Chat/Bubble.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="chat-bubble w-fit max-w-[70%] mt-2 px-2 pt-2 pb-1 rounded-lg text-black relative" :class="[
+    chat?.sent_by_code === user?.id ? 'ml-auto sent' : 'received',
+    chat?.message_type === 'TEXT' ? (chat?.sent_by_code === user?.id ? 'bg-blue-300/60' : 'bg-slate-200') : 'bg-none',
+  ]">
+    <div v-if="activeChatDetails?.chat_room?.participant?.length > 2" class="mb-1 flex gap-2 items-center">
+      <Icon :name="getRoleIcon(chat.sent_by_code)" :size="'1.5rem'" class="text-sm text-slate-500" />
+      <div class="font-bold text-sm text-slate-700">
+        {{
+          activeChatDetails?.chat_room?.participant.find((participant) => participant.user_id === chat.sent_by_code)
+            ?.display_name || 'Unknown User'
+        }}
+      </div>
+    </div>
+    <template v-if="chat?.message_type === 'TEXT'">
+      <div class="break-words">
+        {{ chat?.message }}
+      </div>
+    </template>
+    <template v-if="chat?.message_type === 'IMAGE'">
+      <img :src="chat?.message_attachment" alt="image"
+        class="w-[260px] h-[260px] object-cover rounded-lg hover:cursor-pointer" @click="
+          () => {
+            selectedAttachmentUrl = chat?.message_attachment
+            showModalAttachment = true
+          }
+        " />
+    </template>
+    <div class="text-[10px] text-slate-600 mt-1 text-right">
+      {{ formatDatetime(chat.created_at) }}
+    </div>
+  </div>
+
+  <!--* Show Attachment Modal -->
+  <modals v-model="showModalAttachment" modal-title="Attachment" :footer="false" :on-close-modal="() => {
+    selectedAttachmentUrl = ''
+    showModalAttachment = false
+  }
+    ">
+    <template #modal-content>
+      <div class="w-[800px]">
+        <div class="w-full h-[600px] bg-contain flex bg-center bg-no-repeat justify-center items-center" :style="{
+          'background-image': `url('${selectedAttachmentUrl}')`,
+        }"></div>
+      </div>
+    </template>
+  </modals>
+</template>
+
+<script setup lang="ts">
+import type { Chat, ChatRoomDetails } from '~/models/chat'
+
+interface Props {
+  chat: Chat
+  activeChatDetails: ChatRoomDetails
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+
+const { user } = useAuthStore()
+
+function getRoleIcon(userId: string) {
+  if (userId.startsWith('drivers:')) {
+    return 'moped'
+  } else if (userId.startsWith('merchants:')) {
+    return 'store'
+  } else {
+    return 'uil:house-user'
+  }
+}
+
+const showModalAttachment = ref(false)
+const selectedAttachmentUrl = ref('')
+
+console.log(props.activeChatDetails);
+</script>
+
+<style scoped>
+.chat-bubble::after {
+  content: '';
+  position: absolute;
+  top: 0.5rem;
+  width: 0;
+  height: 0;
+}
+
+.chat-bubble.received::after {
+  left: -8px;
+  border-bottom: 14px solid transparent;
+  border-right: 14px solid #e2e8f0; /* warna bg-slate-200 */
+}
+
+.chat-bubble.sent::after {
+  right: -8px;
+  border-bottom: 14px solid transparent;
+  border-left: 14px solid #bedcfe; /* warna bg-blue-300/60 */
+}
+
+</style>
---
+++ components/Chat/RoomInfo.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    class="relative w-full flex items-center border border-white gap-4 p-2 duration-300 rounded-lg hover:cursor-pointer hover:bg-blue-300/50"
+    :class="[activeChatData?.id === chatRoomData?.id ? 'bg-blue-200/50 !border-blue-300' : '']" @click="selectChatRoom">
+    <div
+      class="flex w-12 h-12 aspect-square items-center justify-center rounded-full bg-blue-300 dark:bg-gray-700 uppercase">
+      <span>{{ chatRoomData?.participant[getOtherParticipantIndex()]?.display_name[0] }}</span>
+    </div>
+    <div class="w-full overflow-hidden">
+      <template v-if="chatRoomData?.participant && chatRoomData.participant.length > 2">
+        <div class="w-full truncate font-bold">
+          {{
+            chatRoomData?.participant
+              .filter((p) => p.display_name !== user?.name)
+              .map((p) => p.display_name)
+              .join(', ')
+          }}
+        </div>
+      </template>
+      <div v-else class="w-full truncate font-semibold flex items-center gap-2">
+        <Icon :name="getRoleIcon(chatRoomData?.participant[getOtherParticipantIndex()]?.user_id)"
+          class="text-sm text-slate-500" />
+        {{ chatRoomData?.participant[getOtherParticipantIndex()]?.display_name }}
+      </div>
+      <template v-if="chatRoomData?.last_message">
+        <div class="w-full truncate text-sm">
+          <div v-if="chatRoomData?.last_message?.message_type === 'TEXT'" class="text-slate-600">
+            {{ chatRoomData?.last_message?.message }}
+          </div>
+          <div v-else class="flex gap-1 items-center text-slate-500">
+            <fa-icon icon="image" />
+            <div class="text-sm">Image</div>
+          </div>
+        </div>
+        <div class="absolute top-2 right-2 text-[10px] text-slate-600">
+          {{ formatDatetime(chatRoomData.created_at) }}
+        </div>
+        <div v-if="chatRoomData?.closed_at" class="absolute bottom-2 right-2 capitalize text-green-600">
+          <UITooltip :tooltip-text="chatRoomData?.closed_at ? $t('closed') : $t('active')" direction="bottom-right">
+            <Icon name="hugeicons:toggle-on" />
+            <!-- {{ chatRoomData?.closed_at ? $t('closed') : $t('active') }} -->
+          </UITooltip>
+        </div>
+        <div v-else class="absolute bottom-2 right-2 capitalize text-red-600">
+          <UITooltip :tooltip-text="chatRoomData?.closed_at ? $t('closed') : $t('active')" direction="bottom-right">
+            <Icon name="hugeicons:toggle-off" />
+            <!-- {{ chatRoomData?.closed_at ? $t('closed') : $t('active') }} -->
+          </UITooltip>
+        </div>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import type { ChatRoom } from '~/models/chat'
+
+interface Props {
+  chatRoomData: ChatRoom
+  activeChatData?: ChatRoom | null
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+
+const emits = defineEmits(['updateActiveChat', 'updateActiveChatDetails'])
+
+const { user } = useAuthStore()
+
+function selectChatRoom() {
+  emits('updateActiveChatDetails', null)
+  emits('updateActiveChat', props.chatRoomData)
+}
+
+function getOtherParticipantIndex() {
+  const otherParticipantIndex = props.chatRoomData?.participant?.findIndex((item) => item?.user_id !== user?.id)
+
+  if (otherParticipantIndex > -1) return otherParticipantIndex
+
+  return 0
+}
+
+function getRoleIcon(userId: string) {
+  if (userId.startsWith('customers:')) {
+    return 'hugeicons:shopping-cart-02'
+  } else if (userId.startsWith('merchants:')) {
+    return 'store'
+  } else {
+    return 'hugeicons:shield-user'
+  }
+}
+
+getOtherParticipantIndex()
+console.log(props.chatRoomData?.participant.length)
+</script>
+
+<style scoped></style>
