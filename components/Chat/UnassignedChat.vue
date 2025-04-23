7dd664a4fff4475cddda2dc0d0006961cf1ab201
Rahmi Liza <57374297+rahmiliza@users.noreply.github.com>
2025-04-23 16:27:04 +0800

    Add chat components for unassigned chat list, detail, bubble, and room info

---
+++ components/Chat/UnassignedChat.vue
@@ -0,0 +1,230 @@
+<template>
+  <div class="w-full h-full">
+    <template v-if="pending || adminChatQueuePending || globalLoading">
+      <LoadingIndicator />
+    </template>
+    <div class="w-full h-full flex bg-white overflow-hidden min-w-[800px]">
+      <ChatUnassignedRoomList :list-chat-room="listChatRoom" :active-chat-data="activeChatData" :admin-chat-queue="adminChatQueue"
+        @update-chat-list-loading="toggleChatListLoading" @update-active-chat="handleSetActiveChatData"
+        @update-admin-chat-queue="updateAdminChatQueue" @toggle-global-loading="toggleGlobalLoading"
+        @update-chat-list-data="updateChatListData" @update-active-chat-details="updateActiveChatDetails" />
+      <template v-if="activeChatData && activeChatDetails">
+        <ChatUnassignedDetail :active-chat-data="activeChatData" :active-chat-details="activeChatDetails"
+          :chatting-container-loading="chattingContainerLoading"
+          :active-chat-details-pagination="activeChatDetailsPagination" :list-chat-room="listChatRoom"
+          @update-active-chat="handleSetActiveChatData" @update-active-chat-details="updateActiveChatDetails"
+          @update-chatting-container-loading="toggleChattingContainerLoading"
+          @update-chat-list-data="updateChatListData" @toggle-global-loading="toggleGlobalLoading"
+          @trigger-fetch-chat-room-details="triggerFetchChatRoomDetails" />
+      </template>
+      <template v-else>
+        <pages-manage-customer-chat-queue-counter :admin-chat-queue="adminChatQueue" />
+      </template>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { io } from 'socket.io-client'
+import type { AdminChatQueue, Chat, ChatQueue, ChatRoom, ChatRoomDetails } from '~/models/chat'
+import type { Response, ResponseWithPagination } from '~/models/response'
+
+const { user } = useAuthStore();
+const toast = useToast();
+
+const {
+  public: { socketUrl },
+} = useRuntimeConfig()
+const socket = io(socketUrl)
+
+const globalLoading = ref(false)
+const chattingContainerLoading = ref(false)
+const listChatLoading = ref(false)
+
+const listChatRoom = ref<ChatRoom[]>([])
+const adminChatQueue = ref<AdminChatQueue>()
+
+const activeChatData = ref<ChatRoom | null>()
+const activeChatDetails = ref<ChatRoomDetails>()
+const activeChatDetailsPagination = ref()
+
+function toggleGlobalLoading(loadingValue: boolean) {
+  globalLoading.value = loadingValue
+}
+
+function toggleChatListLoading(loadingValue: boolean) {
+  listChatLoading.value = loadingValue
+}
+
+function toggleChattingContainerLoading(loadingValue: boolean) {
+  chattingContainerLoading.value = loadingValue
+}
+
+function handleSetActiveChatData(chatRoomData: ChatRoom) {
+  if (chatRoomData == null) {
+    activeChatData.value = null
+    return
+  }
+
+  activeChatData.value = { ...chatRoomData }
+  fetchChatRoomDetails()
+}
+
+function updateAdminChatQueue(newAdminChatQueue: AdminChatQueue) {
+  adminChatQueue.value = { ...newAdminChatQueue }
+}
+
+function updateActiveChatDetails(newChatDetails: ChatRoomDetails) {
+  activeChatDetails.value = { ...newChatDetails }
+}
+
+function updateChatListData(newChatListData: ChatRoom[]) {
+  listChatRoom.value = [...newChatListData]
+}
+
+function triggerFetchChatRoomDetails() {
+  fetchChatRoomDetails(activeChatDetailsPagination.value?.next_page_cursor || '')
+}
+
+async function fetchChatRoomDetails(nextCursor: string = '') {
+  if (activeChatData.value == null) return
+
+  chattingContainerLoading.value = true
+
+  try {
+    const { data, error } = await useApi<ResponseWithPagination>(
+      `/admin/chat-rooms/${activeChatData.value?.id}/chats?offset=${nextCursor}&sortBy=created_at&order=DESC`,
+      { method: 'GET' }
+    )
+    if (data.value?.data) {
+      if (nextCursor) {
+        activeChatDetails.value?.chats.push(...data.value?.data)
+      } else {
+        activeChatDetails.value = { chat_room: activeChatData.value, chats: data.value?.data }
+      }
+      console.log(nextCursor)
+      console.log(activeChatDetails.value)
+
+      activeChatDetailsPagination.value = data.value?.pagination
+    } else {
+      const errMsg = error.value?.data?.message ?? 'An Error was Accrued, Please try again'
+      toast.add({ message: errMsg, type: "error" })
+    }
+  } catch (e: any) {
+    const errMsg = e?.value?.data?.message || 'An Error was Accrued, Please try again'
+    toast.add({ message: errMsg, type: "error" })
+  } finally {
+    chattingContainerLoading.value = false
+  }
+}
+
+const { data, pending } = await useApi<Response<ChatRoom[]>>('/admin/chat-rooms/unassigned', {
+  method: 'GET',
+})
+
+const { data: adminChatQueueData, pending: adminChatQueuePending } = await useApi<UpsertResponse<AdminChatQueue>>(
+  '/admin/chats/admin-queues',
+  {
+    method: 'GET',
+  }
+)
+
+watch(
+  [data, adminChatQueueData],
+  (_) => {
+    adminChatQueue.value = adminChatQueueData.value?.data
+    listChatRoom.value = data.value?.data ?? []
+    listChatRoom.value.sort((a, b) => {
+      if (a.status === 'ACTIVE' && b.status !== 'ACTIVE') {
+        return -1
+      } else if (a.status !== 'ACTIVE' && b.status === 'ACTIVE') {
+        return 1
+      } else {
+        return b.last_message.created_at - a.last_message.created_at
+      }
+    });
+    console.log(listChatRoom.value)
+  },
+  { immediate: true }
+)
+
+watch(activeChatData, (_) => {
+  socket.off('receive-message')
+
+  socket.emit('join-room', user?.id)
+
+  if (activeChatData.value) {
+    socket.emit('join-room', activeChatData.value?.id)
+  }
+
+  socket.on('receive-message', (lastMessage: Chat) => {
+    if (activeChatDetails.value?.chat_room?.id !== lastMessage?.chat_room_id) return
+
+    activeChatDetails.value?.chats?.splice(0, 0, lastMessage)
+
+    const lastMessageIndexRoom = listChatRoom.value?.findIndex((item) => item?.id === lastMessage?.chat_room_id)
+
+    if (lastMessageIndexRoom > -1 && listChatRoom.value[lastMessageIndexRoom]) {
+      listChatRoom.value[lastMessageIndexRoom].last_message = { ...lastMessage }
+
+      // Sorting listChatRoom based on last_message.created_at and status
+      listChatRoom.value.sort((a, b) => {
+        if (a.status === 'ACTIVE' && b.status !== 'ACTIVE') {
+          return -1
+        } else if (a.status !== 'ACTIVE' && b.status === 'ACTIVE') {
+          return 1
+        } else {
+          return b.last_message.created_at - a.last_message.created_at
+        }
+      })
+    }
+  })
+})
+
+onMounted(() => {
+  socket.on('connection', () => { })
+
+  socket.emit('join-room', user?.id)
+
+  socket.on('receive-new-message-queue', (newSubmittedMsgQueue: ChatQueue[] | string) => {
+    if (adminChatQueue.value) {
+      if (typeof newSubmittedMsgQueue === 'string') {
+        adminChatQueue.value.queue = JSON.parse(newSubmittedMsgQueue)
+      } else {
+        adminChatQueue.value.queue = [...newSubmittedMsgQueue]
+      }
+    }
+  })
+
+  socket.on('chat-room', (chatRoom: ChatRoom) => {
+    if (listChatRoom.value.findIndex((item) => item?.id === chatRoom?.id) < 0) {
+      listChatRoom.value.push(chatRoom)
+    } else {
+      listChatRoom.value[listChatRoom.value.findIndex((item) => item?.id === chatRoom?.id)] = { ...chatRoom }
+    }
+
+    // Sorting listChatRoom based on last_message.created_at and status
+    listChatRoom.value.sort((a, b) => {
+      if (a.status === 'ACTIVE' && b.status !== 'ACTIVE') {
+        return -1
+      } else if (a.status !== 'ACTIVE' && b.status === 'ACTIVE') {
+        return 1
+      } else {
+        return b.last_message.created_at - a.last_message.created_at
+      }
+    })
+  })
+})
+
+onUnmounted(() => {
+  socket.off('receive-new-message-queue')
+  socket.off('receive-message')
+  socket.disconnect()
+})
+
+onBeforeMount(() => {
+  // checkPermission('chat_room::index')
+})
+</script>
+
+<style scoped></style>
---
+++ components/Chat/UnassignedDetail.vue
@@ -0,0 +1,445 @@
+<template>
+  <div class="flex flex-col grow relative h-full">
+    <template v-if="chattingContainerLoading">
+      <LoadingIndicator />
+    </template>
+
+    <div class=" w-full flex justify-between items-center border-b border-slate-300/50 px-2 py-1.5">
+      <div class="w-full text-lg font-bold truncate ml-2 text-black">
+        <template v-if="activeChatData?.participant && activeChatData.participant.length > 2">
+          {{
+            activeChatData?.participant
+              .filter((p) => p.display_name !== user?.name)
+              .map((p) => p.display_name)
+              .join(', ')
+          }}
+        </template>
+        <template v-else class="text-black">
+          <div class="text-xs font-normal text-gray-400">Customer</div>
+          <div>{{ activeChatData?.participant[getOtherParticipantIndex()]?.display_name }}</div>
+
+        </template>
+      </div>
+
+      <div class="flex items-center gap-2">
+        <!-- <DPermissionGuard v-if="!activeChatData?.closed_at" permission="chat_room::add_participant"> -->
+        <UITooltip tooltip-text="Accept Chat" direction="bottom-right">
+          <Icon name="hugeicons:checkmark-badge-01"
+            class="ml-auto font-bold p-2 rounded-lg text-2xl text-red-500 hover:cursor-pointer hover:brightness-110 active:brightness-90 hover:bg-red-700"
+            @click="handleAssigned" />
+        </UITooltip>
+        <!-- </DPermissionGuard> -->
+        <!-- <DPermissionGuard v-if="!activeChatData?.closed_at" permission="chat_room::finish">
+      <UITooltip tooltip-text="Finish Chat" direction="bottom-right">
+        <Icon name="uil:plus"
+          class="ml-2 font-bold p-2 rounded-lg text-2xl text-red-600 hover:cursor-pointer hover:brightness-110 active:brightness-90 hover:bg-red-300/50"
+          @click="handleFinishChat" />
+      </UITooltip>
+      </DPermissionGuard> -->
+        <UITooltip tooltip-text="Minimize Chat" direction="bottom-right">
+          <Icon name="hugeicons:cancel-01"
+            class="font-bold p-2 rounded-lg text-2xl text-red-500 hover:cursor-pointer hover:brightness-110 active:brightness-90 hover:bg-red-700"
+            @click="
+              () => {
+                emits('updateActiveChat', null)
+              }
+            " />
+        </UITooltip>
+      </div>
+    </div>
+    <div class="w-full flex flex-col flex-1">
+      <div ref="chatList" v-scroll="onScroll" class="h-full w-full overflow-y-auto flex flex-col p-4 gap-2">
+        <template v-for="chat in activeChatDetails?.chats" :key="chat?.id">
+          <ChatUnassignedBubble :chat="chat" :active-chat-details="activeChatDetails" />
+        </template>
+      </div>
+      <!-- <DPermissionGuard v-if="!activeChatData?.closed_at" permission="chat::store">
+        <div class="flex gap-1 items-center pl-2">
+          <Menu>
+            <div>
+              <MenuButton class="inline-flex w-full relative">
+                <fa-icon icon="paperclip"
+                  class="font-bold p-2 rounded-lg text-xl text-red-600 hover:cursor-pointer hover:brightness-110 active:brightness-90 hover:bg-red-300/50" />
+              </MenuButton>
+            </div>
+
+            <transition enter-active-class="transition duration-100 ease-out"
+              enter-from-class="transform scale-95 opacity-0" enter-to-class="transform scale-100 opacity-100"
+              leave-active-class="transition duration-75 ease-in" leave-from-class="transform scale-100 opacity-100"
+              leave-to-class="transform scale-95 opacity-0">
+              <MenuItems
+                class="absolute z-50 left-[12px] bottom-[48px] mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                <div class="px-1 py-1">
+                  <MenuItem v-slot="{ active }">
+                  <button :class="[
+                    active ? 'bg-red-500 text-white' : 'text-black',
+                    'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                  ]" @click="triggerFileInputClick">
+                    <fa-icon icon="image" class="mr-2" :class="active ? 'bg-red-500 text-white' : 'text-red-600'" />
+                    Image
+                  </button>
+                  </MenuItem>
+                </div>
+              </MenuItems>
+            </transition>
+          </Menu>
+          <input v-model="message" placeholder="Enter Message"
+            class="w-full p-2 bg-slate-100 rounded-lg text-black border border-slate-400 resize-none overflow-hidden max-h-20 text-sm outline-none focus:border-red-500"
+            rows="1" @keydown.enter="() => handleRequestSendMessage('TEXT')" />
+        </div>
+      </DPermissionGuard> -->
+    </div>
+  </div>
+  <div v-if="uploadedFiles.length > 0"
+    class="absolute flex items-center justify-center z-50 top-0 left-0 w-screen h-screen bg-slate-500/50 px-20">
+    <div class="relative flex flex-col w-fit h-fit bg-white rounded-lg justify-between shadow-xl">
+      <div v-if="uploadedFiles[0].loading" class="w-full h-full flex justify-center items-center">
+        <LoadingIndicator />
+      </div>
+      <div class="mt-4 pr-4 flex items-center justify-end">
+        <fa-icon icon="xmark" class="text-2xl hover:cursor-pointer hover:text-red-500 active:text-red-700" @click="
+          () => {
+            uploadedFiles = []
+          }
+        " />
+      </div>
+      <div
+        class="mt-4 bg-slate-200 w-[500px] min-h-[400px] bg-contain bg-no-repeat flex overflow-hidden bg-center justify-center items-center"
+        :style="{
+          'background-image': `url('${uploadedFiles[0]?.url}')`,
+        }"></div>
+      <div class="mt-4 pb-4 pr-4 flex items-center justify-end">
+        <button
+          class="text-white bg-red-700 hover:bg-red-800 focus:ring-2 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 w-28"
+          @click="() => handleRequestSendMessage('IMAGE')">
+          Send
+        </button>
+      </div>
+    </div>
+  </div>
+  <input ref="fileInput" type="file" accept="image/*" :multiple="false" class="hidden" @input="handleFileInput" />
+
+  <!-- * Modal Adding Participant -->
+  <modals v-model="openModalAddNewParticipant" modal-title="Add Participant" :show-overflow="true"
+    :on-ok="handleOkAddNewParticipant" :on-close-modal="handleCloseModalAddNewParticipant"
+    :disabled-btn-ok="!selectedParticipantToAddToChat">
+    <template #modal-content>
+      <div class="w-[400px] h-[80px] select-none overflow-y-visible">
+        <template v-if="loadingGetListAdmin">
+          <shell-indicator-skeleton />
+        </template>
+        <template v-else>
+          <div class="mt-2 mb-1 text-gray-500 text-sm">Admin</div>
+          <input-select v-model="selectedParticipantToAddToChat" placeholder="Choose one Admin"
+            :options="listAdminOptions" />
+        </template>
+      </div>
+    </template>
+  </modals>
+
+  <UIConfirmModal v-model="showConfirm" title="Chat Acceptance Confirmation" message="Will you accept this chat?"
+    @confirm="handleOkAddNewParticipant"/>
+</template>
+
+<script setup lang="ts">
+
+import { io } from 'socket.io-client'
+
+import { vScroll } from '@vueuse/components'
+import type { UseScrollReturn } from '@vueuse/core'
+
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+
+
+import type {
+  ChatRoom,
+  ChatRoomDetails,
+  Chat,
+  UpsertResponse,
+  CursorPaginationResponse,
+  TemporaryFileUpload,
+  User,
+  Option,
+} from '~/models/chat'
+
+interface Props {
+  activeChatData?: ChatRoom | null
+  activeChatDetails: ChatRoomDetails
+  activeChatDetailsPagination?: CursorPaginationResponse
+  listChatRoom?: ChatRoom[]
+  chattingContainerLoading: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+
+const emits = defineEmits([
+  'updateChattingContainerLoading',
+  'updateActiveChatDetails',
+  'triggerFetchChatRoomDetails',
+  'updateActiveChat',
+  'updateChatListData',
+  'toggleGlobalLoading',
+])
+
+const { user } = useAuthStore();
+const toast = useToast();
+const showConfirm = ref(false)
+
+const {
+  public: { socketUrl },
+} = useRuntimeConfig()
+const socket = io(socketUrl)
+
+const loadingGetListAdmin = ref(false)
+
+const listAdminOptions = ref<Option[]>([])
+
+const chatList = ref<HTMLElement | null>(null)
+const fileInput = ref<HTMLInputElement | null>(null)
+const message = ref()
+
+const uploadedFiles = ref<TemporaryFileUpload[]>([])
+
+const openModalAddNewParticipant = ref(false)
+
+const selectedParticipantToAddToChat = ref()
+
+let debounceTimeout: ReturnType<typeof setTimeout> | null = null
+
+function onScroll(state: UseScrollReturn) {
+  if (debounceTimeout) {
+    clearTimeout(debounceTimeout)
+  }
+
+  debounceTimeout = setTimeout(() => {
+    const { arrivedState } = state
+    const { top } = arrivedState
+
+    if (top && props?.activeChatDetailsPagination?.has_next) {
+      emits('triggerFetchChatRoomDetails')
+    }
+  }, 200) // Adjust the debounce delay as needed
+}
+
+function triggerFileInputClick() {
+  fileInput.value?.click()
+}
+
+const handleAssigned = () => {
+  if (props?.activeChatData?.closed_at) return
+
+  showConfirm.value = true
+}
+
+async function handleFileInput(event: Event) {
+  const tempId = Math.random().toString(36).substr(2)
+  const target = event.target as HTMLInputElement
+
+  if (target.files && target.files[0]) {
+    uploadedFiles.value = []
+    uploadedFiles.value = [
+      {
+        id: tempId,
+        type: 'x-temporary',
+        loading: true,
+      },
+    ]
+
+    const payload = new FormData()
+
+    payload.append('file', target.files[0])
+
+    try {
+      const { data } = await useApi<UpsertResponse<TemporaryFileUpload>>('/storage/temporary', {
+        method: 'POST',
+        body: payload,
+      })
+
+      uploadedFiles.value[0].url = data.value?.data?.url
+      uploadedFiles.value[0].id = data.value?.data?.id
+    } catch (error) {
+      uploadedFiles.value[0].error = true
+    } finally {
+      uploadedFiles.value[0].loading = false
+    }
+  }
+
+  target.value = ''
+}
+
+function emitLoading(isLoading: boolean) {
+  emits('updateChattingContainerLoading', isLoading)
+}
+
+function getOtherParticipantIndex() {
+  if (!props.activeChatData) return 0
+
+  const otherParticipantIndex = props?.activeChatData?.participant?.findIndex((item) => item?.user_id !== user?.id)
+
+  if (otherParticipantIndex > -1) return otherParticipantIndex
+
+  return 0
+}
+
+async function handleAddingParticipant() {
+  selectedParticipantToAddToChat.value = ''
+  openModalAddNewParticipant.value = true
+  loadingGetListAdmin.value = true
+  try {
+    const { data, error } = await useApi<UpsertResponse<User[]>>('/admins?per_page=1000', {
+      method: 'GET',
+    })
+
+    if (data.value?.data) {
+      listAdminOptions.value =
+        data.value?.data?.map((item: User) => {
+          const isParticipant = props.activeChatData?.participant.some((participant) => participant.user_id === item.id)
+          return { value: item?.id ?? '', label: item?.name ?? '', disabled: isParticipant }
+        }) ?? []
+    } else {
+      const errMsg = error.value?.data?.message ?? 'Failed to fetch admin list, please try again'
+      toast.add({ message: errMsg, type: "error" })
+    }
+  } catch (e: any) {
+    const errMsg = e?.value?.data?.message || 'Failed to fetch admin list, please try again'
+    toast.add({ message: errMsg, type: "error" })
+  } finally {
+    loadingGetListAdmin.value = false
+  }
+}
+
+function handleCloseModalAddNewParticipant() {
+  openModalAddNewParticipant.value = false
+}
+
+async function handleOkAddNewParticipant() {
+  emitLoading(true)
+
+  try {
+    const { data, error } = await useApi<UpsertResponse<ChatRoom>>('/admin/chat-rooms/' + props?.activeChatData?.id + '/chat-participant', {
+      method: 'POST',
+      body: {
+        room_id: props?.activeChatData?.id ?? '',
+        users: [user],
+      },
+    })
+
+    if (data.value?.ok) {
+      toast.add({ message: 'Participant added successfully', type: "success" })
+
+      const chatIndex = props?.listChatRoom?.findIndex((item) => item?.id === props?.activeChatDetails?.chat_room?.id)
+
+      if (chatIndex !== undefined && chatIndex > -1 && props.listChatRoom) {
+        const tempListChat = [...props.listChatRoom]
+        tempListChat[chatIndex] = { ...tempListChat[chatIndex], ...data.value?.data }
+
+        // emits('updateChatListData', tempListChat)
+        // emits('triggerFetchChatRoomDetails')
+      }
+    } else {
+      const errMsg = error.value?.data?.message ?? 'Failed to add participant, please try again'
+      toast.add({ message: errMsg, type: "error" })
+    }
+  } catch (e: any) {
+    const errMsg = e?.value?.data?.message || 'Failed to add participant, please try again'
+    toast.add({ message: errMsg, type: "error" })
+  } finally {
+    emitLoading(false)
+    openModalAddNewParticipant.value = false
+  }
+}
+
+async function handleRequestSendMessage(message_type: 'TEXT' | 'IMAGE' = 'TEXT') {
+  let chatMessage = message.value
+
+  if (message_type === 'IMAGE') {
+    chatMessage = uploadedFiles.value[0]?.id
+    uploadedFiles.value = []
+  }
+
+  emitLoading(true)
+
+  try {
+    const otherParticipantId = props?.activeChatData?.participant[getOtherParticipantIndex()]?.user_id
+
+    const { data, error } = await useApi<UpsertResponse<Chat>>(`admin/chat-rooms/${props?.activeChatData?.id}/chat`, {
+      method: 'POST',
+      body: {
+        message: chatMessage ?? '',
+        chat_room_id: props?.activeChatData?.id ?? '',
+        message_type: message_type ?? 'TEXT',
+      },
+    })
+
+    if (data.value?.status) {
+      message.value = null
+
+      const roomValue = {
+        ...props.activeChatDetails?.chat_room,
+        last_message: data.value?.data,
+      }
+
+      socket.emit('send-message', roomValue, props.activeChatData?.id, otherParticipantId)
+    } else {
+      const errMsg = error.value?.data?.message ?? 'An Error was Accrued, Please try again'
+      toast.add({ message: errMsg, type: "error" })
+    }
+  } catch (e: any) {
+    const errMsg = e?.value?.data?.message || 'An Error was Accrued, Please try again'
+    toast.add({ message: errMsg, type: "error" })
+  } finally {
+    emitLoading(false)
+  }
+}
+
+async function handleFinishChat() {
+  emits('toggleGlobalLoading', true)
+
+  try {
+    const { data, error } = await useApi<UpsertResponse<ChatRoom>>('/chats/finish', {
+      method: 'PUT',
+      body: {
+        room_id: props.activeChatDetails?.chat_room?.id,
+      },
+    })
+
+    if (data.value?.ok) {
+      const chatIndex = props?.listChatRoom?.findIndex((item) => item?.id === props?.activeChatDetails?.chat_room?.id)
+
+      if (chatIndex !== undefined && chatIndex > -1 && props.listChatRoom) {
+        socket.emit(
+          'finish-chat-room',
+          props?.activeChatDetails?.chat_room?.id,
+          props?.activeChatDetails?.chat_room?.participant?.[getOtherParticipantIndex()]?.user_id ?? ''
+        )
+
+        const tempListChat = [...props.listChatRoom]
+        tempListChat[chatIndex] = { ...tempListChat[chatIndex], ...data.value?.data }
+
+        // Sorting listChatRoom based on last_message.created_at and status
+        tempListChat.sort((a, b) => {
+          if (a.status === 'ACTIVE' && b.status !== 'ACTIVE') {
+            return -1
+          } else if (a.status !== 'ACTIVE' && b.status === 'ACTIVE') {
+            return 1
+          } else {
+            return b.last_message.created_at - a.last_message.created_at
+          }
+        })
+        emits('updateChatListData', tempListChat)
+        emits('updateActiveChat', null)
+      }
+    } else {
+      const errMsg = error.value?.data?.message ?? 'An Error was Accrued, Please try again'
+      toast.add({ message: errMsg, type: "error" })
+    }
+  } catch (e: any) {
+    const errMsg = e?.value?.data?.message || 'An Error was Accrued, Please try again'
+    toast.add({ message: errMsg, type: "error" })
+  } finally {
+    emits('toggleGlobalLoading', false)
+  }
+}
+</script>
+
+<style scoped></style>
