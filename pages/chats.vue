d42e2d9eff61193870efd410487a2c49a99a3d7a
rahmiliza <57374297+rahmiliza@users.noreply.github.com>
2025-05-06 10:22:11 +0800

    Refactor chats.vue: improve layout structure, enhance button styles, and maintain component references for better clarity and usability

--- pages/chats.vue
+++ pages/chats.vue
@@ -1,30 +1,51 @@
 <template>
-    <div class="flex flex-col w-full h-full -mt-5">
-      <div class="flex border-b-2 border-slate-400">
+  <div class="flex flex-col w-full h-full -mt-5">
+    <div class="flex justify-between border-b-2 border-slate-400">
+      <div>
         <button v-for="(tab, index) in tabs" :key="index" @click="activeTab = index" :class="[
-          'relative px-4 py-2 font-bold ',
+          'relative px-4 py-2 font-bold text-base ',
           activeTab === index
-            ? 'border-b-2 border-blue-500 text-blue-600 text-xl'
-            : 'text-gray-500 hover:text-blue-500 text-lg'
+            ? 'border-b-2 border-blue-500 text-blue-600 -mb-[2px]'
+            : 'text-gray-500 hover:text-blue-500'
         ]">
           {{ tab.label }}
           <!-- <div class="absolute top-0 right-0 rounded-full bg-red-500 text-white text-[8px] flex items-center justify-center px-0.5">242</div> -->
         </button>
       </div>
-  
-      <component :is="tabs[activeTab].component" />
+      <div>
+
+        <Icon name="uil:comment-plus" size="1.8rem"
+          class="font-extrabold p-2 rounded-lg text-2xl text-slate-600 hover:cursor-pointer hover:brightness-200 active:brightness-90 hover:bg-red-900"
+          @click="handleCreateChat" />
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-  import ActiveChat from '~/components/Chat/ActiveChat.vue'
-  import HistoryChat from '~/components/Chat/HistoryChat.vue'
-  import UnassignedChat from '~/components/Chat/UnassignedChat.vue'
-  
-  const activeTab = ref(0)
-  const tabs = [
-    { label: 'Active Chats', component: ActiveChat },
-    { label: 'Unassigned Chats', component: UnassignedChat },
-    { label: 'Chat History', component: HistoryChat },
-  ]
-  </script>
+
+    <ActiveChat ref="activeChatRef" v-show="activeTab === 0" />
+    <UnassignedChat ref="unassignedChatRef"v-show="activeTab === 1"  />
+    <HistoryChat v-if="activeTab === 2" ref="historyChatRef" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import ActiveChat from '~/components/Chat/ActiveChat.vue'
+import HistoryChat from '~/components/Chat/HistoryChat.vue'
+import UnassignedChat from '~/components/Chat/UnassignedChat.vue'
+
+const activeTab = ref(0)
+
+const activeChatRef = ref<InstanceType<typeof ActiveChat> | null>(null)
+// const unassignedChatRef = ref<InstanceType<typeof UnassignedChat> | null>(null)
+// const historyChatRef = ref<InstanceType<typeof HistoryChat> | null>(null)
+
+const tabs = [
+  { label: 'Active Chats' },
+  { label: 'Unassigned Chats' },
+  { label: 'Chat History' },
+]
+
+const handleCreateChat = async () => {
+  // await nextTick()
+  // console.log(activeChatRef.value?.$refs)
+  activeChatRef.value?.$refs?.roomListRef?.handleCreateChat()
+}
+</script>
