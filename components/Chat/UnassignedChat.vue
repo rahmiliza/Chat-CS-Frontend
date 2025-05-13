48184edcc082047f894b22a28359084b7dbc7f1b
rahmiliza <57374297+rahmiliza@users.noreply.github.com>
2025-05-13 15:56:12 +0800

    Refactor UnassignedChat component: update loading state checks, streamline chat room fetching, and enhance sorting logic

--- components/Chat/UnassignedChat.vue
+++ components/Chat/UnassignedChat.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="w-full  h-[calc(100%-34px)]">
-    <template v-if="pending || adminChatQueuePending || globalLoading">
+    <template v-if="chatRoomsStatus === 'pending' || globalLoading">
       <LoadingIndicator />
     </template>
     <div class="w-full h-full flex bg-white overflow-hidden min-w-[800px]">
-      <ChatUnassignedRoomList :list-chat-room="listChatRoom" :active-chat-data="activeChatData" :admin-chat-queue="adminChatQueue"
-        @update-chat-list-loading="toggleChatListLoading" @update-active-chat="handleSetActiveChatData"
-        @update-admin-chat-queue="updateAdminChatQueue" @toggle-global-loading="toggleGlobalLoading"
-        @update-chat-list-data="updateChatListData" @update-active-chat-details="updateActiveChatDetails" />
+      <ChatUnassignedRoomList :list-chat-room="listChatRoom" :active-chat-data="activeChatData"
+        :admin-chat-queue="adminChatQueue" @update-chat-list-loading="toggleChatListLoading"
+        @update-active-chat="handleSetActiveChatData" @update-admin-chat-queue="updateAdminChatQueue"
+        @toggle-global-loading="toggleGlobalLoading" @update-chat-list-data="updateChatListData"
+        @update-active-chat-details="updateActiveChatDetails" />
       <template v-if="activeChatData && activeChatDetails">
         <ChatUnassignedDetail :active-chat-data="activeChatData" :active-chat-details="activeChatDetails"
           :chatting-container-loading="chattingContainerLoading"
@@ -47,6 +48,24 @@ const adminChatQueue = ref<AdminChatQueue>()
 const activeChatData = ref<ChatRoom | null>()
 const activeChatDetails = ref<ChatRoomDetails>()
 const activeChatDetailsPagination = ref()
+
+const { data: chatRooms, status: chatRoomsStatus } = await useAsyncData('chatRooms', () =>
+  useApi<Response<ChatRoom[]>>('/new/admin/chat-rooms?unassigned=true', {
+    method: 'GET',
+  }).then((res) => res.data.value?.data)
+)
+
+watchEffect(() => {
+  if (Array.isArray(chatRooms.value)) {
+    listChatRoom.value = [...chatRooms.value].sort(sortChatRoom)
+  }
+})
+
+function sortChatRoom(a: ChatRoom, b: ChatRoom) {
+  if (a.status === 'ACTIVE' && b.status !== 'ACTIVE') return -1
+  if (a.status !== 'ACTIVE' && b.status === 'ACTIVE') return 1
+  return b.last_message.created_at - a.last_message.created_at
+}
 
 function toggleGlobalLoading(loadingValue: boolean) {
   globalLoading.value = loadingValue
@@ -93,7 +112,7 @@ async function fetchChatRoomDetails(nextCursor: string = '') {
 
   try {
     const { data, error } = await useApi<ResponseWithPagination>(
-      `/admin/chat-rooms/${activeChatData.value?.id}/chats?offset=${nextCursor}&sortBy=created_at&order=DESC`,
+      `/new/admin/chat-rooms/${activeChatData.value?.id}/chats?offset=${nextCursor}&sortBy=created_at&order=DESC`,
       { method: 'GET' }
     )
     if (data.value?.data) {
@@ -118,35 +137,6 @@ async function fetchChatRoomDetails(nextCursor: string = '') {
   }
 }
 
-const { data, pending } = await useApi<Response<ChatRoom[]>>('/admin/chat-rooms/unassigned', {
-  method: 'GET',
-})
-
-const { data: adminChatQueueData, pending: adminChatQueuePending } = await useApi<UpsertResponse<AdminChatQueue>>(
-  '/admin/chats/admin-queues',
-  {
-    method: 'GET',
-  }
-)
-
-watch(
-  [data, adminChatQueueData],
-  (_) => {
-    adminChatQueue.value = adminChatQueueData.value?.data
-    listChatRoom.value = data.value?.data ?? []
-    listChatRoom.value.sort((a, b) => {
-      if (a.status === 'ACTIVE' && b.status !== 'ACTIVE') {
-        return -1
-      } else if (a.status !== 'ACTIVE' && b.status === 'ACTIVE') {
-        return 1
-      } else {
-        return b.last_message.created_at - a.last_message.created_at
-      }
-    });
-    console.log(listChatRoom.value)
-  },
-  { immediate: true }
-)
 
 watch(activeChatData, (_) => {
   socket.off('receive-message')
