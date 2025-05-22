3a90e3e082a903cfb130d175a0db6b5ea6aef5fe
liza <rahmiliza27@gmail.com>
2025-05-22 12:54:08 +0800

    Fix typo in UIConfirmModal message for chat acceptance confirmation

--- components/Chat/UnassignedDetail.vue
+++ components/Chat/UnassignedDetail.vue
@@ -102,7 +102,7 @@
     </template>
   </UIModals>
 
-  <UIConfirmModal v-model="showConfirm" class="text-lg font-bold" title="Chat Acceptance Confirmation" message="Will you accept this chat?"
+  <UIConfirmModal v-model="showConfirm" class="text-lg font-bold" title="Chat Acceptance Confirmation" message="Will you accept this Chat Room?"
     @confirm="handleOkAddNewParticipant"/>
 </template>
 
