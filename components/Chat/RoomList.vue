d7b32200dbf6d8c7c59ec2d0b4dd1a485798e061
rahmiliza <57374297+rahmiliza@users.noreply.github.com>
2025-05-26 15:08:15 +0800

    Search query update for filter Chat Room

--- components/Chat/RoomList.vue
+++ components/Chat/RoomList.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="w-96 h-full flex flex-col border-r border-slate-300/50">
-    <!-- <div class="h-14 flex justify-between items-center border-b-2 border-slate-400 px-2 pb-1">
-      <div class="text-lg font-medium text-slate-500 my-5">
-        Direct Message
+  <div class="w-96 h-full flex flex-col border-r border-slate-300/70">
+    <div
+      class="w-full h-[49px] flex justify-between items-center px-2 py-1 border-b border-slate-300/50 bg-blue-400/20 dark:text-black">
+      <div class="relative w-full">
+        <div @click="handleSearch" class="absolute inset-y-0 end-0 flex items-center pe-3 cursor-pointer">
+          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+            fill="none" viewBox="0 0 20 20">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+          </svg>
+        </div>
+        <input type="search" v-model="searchQuery" @keyup.enter.prevent="handleSearch"
+          class="block w-full p-2.5 pe-10 text-sm text-gray-900 border-0 rounded-lg bg-transparent outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search chat" />
       </div>
-
-      <DPermissionGuard permission="chat_room::assign">
-        <Icon name="uil:comment-plus" size="1.8rem"
-          class="font-extrabold p-2 rounded-lg text-2xl text-slate-600 hover:cursor-pointer hover:brightness-200 active:brightness-90 hover:bg-red-900"
-          @click="handleCreateChat" />
-      </DPermissionGuard>
-    </div> -->
+    </div>
     <div class="h-full my-4 pr-4 flex flex-col gap-2.5 overflow-y-auto">
-      <template v-if="listChatRoom?.length > 0">
-        <template v-for="chatRoomData in listChatRoom" :key="chatRoomData?.id" class="dark:text-black text-black">
+      <template v-if="filteredChatRooms?.length > 0">
+        <template v-for="chatRoomData in filteredChatRooms" :key="chatRoomData?.id" class="dark:text-black text-black">
           <ChatRoomInfo :chat-room-data="chatRoomData" :active-chat-data="activeChatData"
             @update-active-chat="$emit('update-active-chat', $event)"
             @update-active-chat-details="$emit('updateActiveChatDetails', $event)"
@@ -21,7 +25,7 @@
         </template>
       </template>
       <template v-else>
-        <div class="flex justify-center items-center h-full text-gray-400">{{ $t ('Chat.no-chat')}}</div>
+        <div class="flex justify-center items-center h-full text-gray-400">{{ $t('Chat.no-chat') }}</div>
       </template>
     </div>
   </div>
@@ -57,7 +61,6 @@ interface Option {
   disabled?: boolean
 }
 
-
 const { user } = useAuthStore();
 const toast = useToast();
 
@@ -82,7 +85,7 @@ const emits = defineEmits([
   'updateActiveChatDetails',
   'updateChattingContainerLoading',
 ])
-
+const searchQuery = ref('');
 const {
   public: { socketUrl },
 } = useRuntimeConfig()
@@ -100,7 +103,7 @@ async function handleAddAdminChatIndex() {
   ) {
     return
   }
-// list for chat room
+  // list for chat room
   try {
     const { data, error } = await useApi<Response<AdminChatQueueExtended>>('new/admin/chat-rooms', {
       method: 'POST',
@@ -110,15 +113,8 @@ async function handleAddAdminChatIndex() {
       const tempListChatRoom = [...props.listChatRoom]
       tempListChatRoom.push(data.value?.data?.assigned_chat_room)
 
-      // Sorting listChatRoom based on last_message.created_at and status
       tempListChatRoom.sort((a, b) => {
-        if (a.status === 'ACTIVE' && b.status !== 'ACTIVE') {
-          return -1
-        } else if (a.status !== 'ACTIVE' && b.status === 'ACTIVE') {
-          return 1
-        } else {
-          return b.last_message.created_at - a.last_message.created_at
-        }
+        return b.last_message.created_at - a.last_message.created_at
       })
 
       const otherParticipantIndex =
@@ -187,7 +183,6 @@ async function handleOkCreateChat() {
       body: {
         participants: [
           { id: selectedParticipantToAddToChat.value ?? '' },
-          // { id: user?.id, display_name: user?.name }
         ]
       },
     })
@@ -219,6 +214,20 @@ async function handleOkCreateChat() {
     emitLoading(false)
     openModalCreateChat.value = false
   }
+}
+
+const filteredChatRooms = computed(() => {
+  if (!searchQuery.value) return props.listChatRoom;
+
+  return props.listChatRoom.filter((chatRoom) => {
+    return chatRoom.participant?.some((p) =>
+      p?.display_name?.toLowerCase().includes(searchQuery.value.toLowerCase())
+    );
+  });
+});
+
+function handleSearch() {
+  // console.log('Search query:', searchQuery.value);
 }
 
 defineExpose({
