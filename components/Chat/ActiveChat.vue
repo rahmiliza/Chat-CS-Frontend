7af75c1b0d4fdd29d5029a9fdecaeab23b614cbc
rahmiliza <57374297+rahmiliza@users.noreply.github.com>
2025-05-28 09:48:16 +0800

    Refactor ActiveChat component by removing unused admin chat queue logic and related code for improved clarity

--- components/Chat/ActiveChat.vue
+++ components/Chat/ActiveChat.vue
@@ -5,8 +5,7 @@
     </template>
     <div class="w-full h-full flex bg-white overflow-hidden min-w-[800px]">
       <ChatRoomList ref="roomListRef" :list-chat-room="listChatRoom" :active-chat-data="activeChatData"
-        :admin-chat-queue="adminChatQueue" @update-chat-list-loading="toggleChatListLoading"
-        @update-active-chat="handleSetActiveChatData" @update-admin-chat-queue="updateAdminChatQueue"
+        @update-chat-list-loading="toggleChatListLoading" @update-active-chat="handleSetActiveChatData"
         @toggle-global-loading="toggleGlobalLoading" @update-chat-list-data="updateChatListData"
         @update-active-chat-details="updateActiveChatDetails" />
       <template v-if="activeChatData && activeChatDetails">
@@ -19,7 +18,6 @@
           @trigger-fetch-chat-room-details="triggerFetchChatRoomDetails" />
       </template>
       <template v-else>
-        <!-- <pages-manage-customer-chat-queue-counter :admin-chat-queue="adminChatQueue" /> -->
       </template>
     </div>
   </div>
@@ -27,7 +25,7 @@
 
 <script setup lang="ts">
 import { io } from 'socket.io-client'
-import type { AdminChatQueue, Chat, ChatQueue, ChatRoom, ChatRoomDetails, Participant } from '~/models/chat'
+import type { Chat, ChatRoom, ChatRoomDetails } from '~/models/chat'
 import type { Response, ResponseWithPagination } from '~/models/response'
 import RoomList from './RoomList.vue';
 
@@ -44,7 +42,6 @@ const chattingContainerLoading = ref(false)
 const listChatLoading = ref(false)
 
 const listChatRoom = ref<ChatRoom[]>([])
-const adminChatQueue = ref<AdminChatQueue>()
 
 const activeChatData = ref<ChatRoom | null>()
 const activeChatDetails = ref<ChatRoomDetails>()
@@ -60,38 +57,9 @@ const { data: chatRooms, status: chatRoomsStatus } = await useAsyncData('chatRoo
 
 watchEffect(async () => {
   if (Array.isArray(chatRooms.value)) {
-    // await fetchChatRoomDetailsForAll(chatRooms.value)
     listChatRoom.value = [...chatRooms.value].sort(sortChatRoom)
   }
 })
-
-// async function fetchChatRoomDetailsForAll(chatRooms: ChatRoom[]) {
-//   try {
-//     const chatRoomsWithDetails = await Promise.all(
-//       chatRooms.map(async (chatRoom) => {
-//         const participantsResponse = await useApi<Response<Participant[]>>(
-//           `/new/admin/chat-rooms/${chatRoom.id}/chat-participant`
-//         );
-//         const messagesResponse = await useApi<ResponseWithPagination>(
-//           `/new/admin/chat-rooms/${chatRoom.id}/chats?page=1&per_page=10`
-//         );
-
-//         return {
-//           ...chatRoom,
-//           participant: participantsResponse.data.value?.data || [],
-//           chats: messagesResponse.data.value?.data || [],
-//           last_message: messagesResponse.data.value?.data[0] || null,
-//         };
-//       })
-//     );
-
-//     listChatRoom.value = chatRoomsWithDetails;
-//     console.log('Chat rooms with details:', listChatRoom);
-//   } catch (e) {
-//     console.error('Error fetching chat room details:', e);
-//     toast.add({ message: 'Failed to fetch chat room details', type: 'error' });
-//   }
-// }
 
 
 function sortChatRoom(a: ChatRoom, b: ChatRoom) {
@@ -121,10 +89,6 @@ function handleSetActiveChatData(chatRoomData: ChatRoom) {
   fetchChatRoomDetails()
 }
 
-function updateAdminChatQueue(newAdminChatQueue: AdminChatQueue) {
-  adminChatQueue.value = { ...newAdminChatQueue }
-}
-
 function updateActiveChatDetails(newChatDetails: ChatRoomDetails) {
   activeChatDetails.value = { ...newChatDetails }
 }
@@ -140,7 +104,6 @@ function triggerFetchChatRoomDetails() {
 async function fetchChatRoomDetails(nextCursor: string = '') {
   if (activeChatData.value == null) return
 
-  console.log('activeChatData', activeChatData)
   chattingContainerLoading.value = true
 
   try {
@@ -154,8 +117,6 @@ async function fetchChatRoomDetails(nextCursor: string = '') {
       } else {
         activeChatDetails.value = { chat_room: activeChatData.value, chats: data.value?.data }
       }
-      console.log(nextCursor)
-      console.log(activeChatDetails.value)
 
       activeChatDetailsPagination.value = data.value?.pagination
     } else {
@@ -189,13 +150,7 @@ watch(activeChatData, (_) => {
     if (lastMessageIndexRoom > -1 && listChatRoom.value[lastMessageIndexRoom]) {
       listChatRoom.value[lastMessageIndexRoom].last_message = { ...lastMessage }
 
-      // Sorting listChatRoom based on last_message.created_at and status
       listChatRoom.value.sort((a, b) => {
-        // if (a.status === 'ACTIVE' && b.status !== 'ACTIVE') {
-        //   return -1
-        // } else if (a.status !== 'ACTIVE' && b.status === 'ACTIVE') {
-        //   return 1
-        // } else {
         return b.last_message.created_at - a.last_message.created_at
         // }
       })
@@ -207,16 +162,6 @@ onMounted(() => {
   socket.on('connection', () => { })
 
   socket.emit('join-room', user?.id)
-
-  socket.on('receive-new-message-queue', (newSubmittedMsgQueue: ChatQueue[] | string) => {
-    if (adminChatQueue.value) {
-      if (typeof newSubmittedMsgQueue === 'string') {
-        adminChatQueue.value.queue = JSON.parse(newSubmittedMsgQueue)
-      } else {
-        adminChatQueue.value.queue = [...newSubmittedMsgQueue]
-      }
-    }
-  })
 
   socket.on('last-message-room', (lastMessage: Chat) => {
     const lastMessageIndexRoom = listChatRoom.value?.findIndex((item) => item?.id === lastMessage?.chat_room_id)
@@ -277,8 +222,9 @@ onMounted(() => {
 })
 
 onUnmounted(() => {
-  socket.off('receive-new-message-queue')
   socket.off('receive-message')
+  socket.off('chat-room')
+  socket.off('last-message-room')
   socket.disconnect()
 })
 
